2c65871c150b9c19908de792c3e7086a00799150
Oscar <oscarlagatta@gmail.com>
2023-04-15 11:14:52 +0100

    Adding public routes

--- src/iam/authentication/guards/access-token/access-token.guard.ts
+++ src/iam/authentication/guards/access-token/access-token.guard.ts
@@ -30,7 +30,6 @@ export class AccessTokenGuard implements CanActivate {
         this.jwtConfiguration,
       );
       request[REQUEST_USER_KEY] = payload;
-      console.log(payload);
     } catch {
       throw new UnauthorizedException();
     }
